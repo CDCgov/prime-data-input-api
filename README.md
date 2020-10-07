7efd0ce3ab71d62eca3a8d2057fd9bd90efe4319
Shelby Switzer <shelby.switzer@cms.hhs.gov>
2020-10-07 11:11:55 -0400

    Initialize project with API, OAS doc, and README
    * API.js serves up a mock server based on the OAS doc
    * OAS doc is version 3.0.0 and includes very rough draft of API
    * README has basic info and setup instructions

---
+++ README.md
@@ -0,0 +1,24 @@
+# PRIME Data Inoput API
+
+This is a prototype API for the PRIME Data Input application. It utilizes a design-first approach to API design and implementation: the [OpenAPI Specification](https://www.openapis.org/) in `openapi.yml` is the design document from which the API is generated. 
+
+The API itself is Express middleware generated with [OpenAPI Enforcer](https://github.com/byu-oit/openapi-enforcer). It currently is a mock server serving data provided as `examples` in the `openapi.yml` document. As we iterate on the design, we will begin building out this middleware so that it implements desired logic and connects to desired data storage, etc.
+
+## Getting Started
+
+Clone this repo. 
+
+Make sure you have NodeJS installed and accessible in the project directory.
+
+Run the server: `node api.js`
+
+Your API is now available at localhost:3000.
+
+## Using the OAS document
+
+If you don't want to use this Express server, or you want to take advantage the extensive tooling ecosystem around OpenAPI, you can simply plug and play `openapi.yml` with any of your favorite tools! This project's OAS doc is in OAS v 3.0.0, so you can use any tool compatible with this version.
+
+Check out these lists of tools:
+
+[OpenAPI.tools](https://openapi.tools/)
+[OpenAPI 3.0 Implementations](https://github.com/OAI/OpenAPI-Specification/blob/master/IMPLEMENTATIONS.md)
---
+++ api.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const Enforcer = require('openapi-enforcer-middleware')
+const path = require('path')
+
+const app = express()
+app.use(express.json())
+
+const controllerDirectory = path.resolve(__dirname, 'controllers')
+const mockDirectory = path.resolve(__dirname, 'mock-controllers') 
+const pathToOpenApiDoc = path.resolve(__dirname, 'openapi.yml')
+
+// Create an enforcer middleware instance
+const enforcer = Enforcer(pathToOpenApiDoc)
+
+// Add mocking middleware to the enforcer middleware.
+// This middleware will handle explicit mock requests.
+enforcer.mocks(mockDirectory, false)
+  .catch(console.error)
+
+// Add controller middleware to the enforcer middleware .
+// This middleware will handle requests for real data.
+enforcer.controllers(controllerDirectory)
+  .catch(console.error)
+
+// Add fallback mocking middleware to the enforcer middleware.
+// This middleware will automatically run mocking if the
+// controller could not produce a response.
+enforcer.mocks(mockDirectory, true)
+  .catch(() => {}) // Any errors will have already been reported by explicit mock middleware
+
+// Add the enforcer middleware runner to the express app.
+app.use(enforcer.middleware())
+
+// Add error handling middleware
+app.use((err, req, res, next) => {
+  // If the error was in the client's request then send back a detailed report
+  if (err.statusCode >= 400 && err.statusCode < 500 && err.exception) {
+    res.set('Content-Type', 'text/plain')
+    res.status(err.statusCode)
+    res.send(err.message)
+
+  // If it's unsafe to send back detailed errors then send back limited error information
+  } else {
+    console.error(err.stack)
+    res.sendStatus(err.statusCode || 500)
+  }
+})
+
+const listener = app.listen(3000, err => {
+  if (err) return console.error(err.stack)
+  console.log('Server listening on port ' + listener.address().port)
+})
